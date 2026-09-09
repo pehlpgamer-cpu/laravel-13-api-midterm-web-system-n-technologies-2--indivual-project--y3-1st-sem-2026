5f08056d7522cd67e9fd45cce38acb4abe05d757
pehlpganer-cpu <pehlpgamer@gmail.com>
2026-09-09 08:06:17 +0800

    - added versioned api routes   - api_v1.php   - api_v2.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,20 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        //api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function()
+        {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(__DIR__ . "/../routes/api_v1.php");
+
+            Route::middleware('api')
+                ->prefix('api/v2')
+                ->group(__DIR__ . "/../routes/api_v2.php");
+        },
+
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prependToPriorityList(
